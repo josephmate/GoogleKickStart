0089b19c0d73ce4bef9f5b54d617b08b11063075
Joseph Mate <mate.joseph@gmail.com>
2019-11-19 21:57:00 -0500

    added objects to navigate the state space

--- src/main/kotlin/com/josephmate/google/kickstart/Problem2Mural.kt
+++ src/main/kotlin/com/josephmate/google/kickstart/Problem2Mural.kt
@@ -21,8 +21,97 @@ class MuralSolver() {
     }
 }
 
-class Problem(numOfDigits: Int, wallBeauty: String) {
+fun encodeStart(numOfDigits: Int, paintPosn: Int): PaintState {
+    val builder: StringBuilder = StringBuilder(numOfDigits)
+    for(i in 1..numOfDigits) {
+        builder.append(' ')
+    }
+    builder.setCharAt(paintPosn, '*')
+    return PaintState(
+            builder.toString(),
+            paintPosn,
+            paintPosn,
+            -1,
+            numOfDigits
+            )
+}
 
+class Problem(val numOfDigits: Int, val wallBeauty: String) {
+}
+
+class PaintState(
+        val stringEncoding: String,
+        val leftMostPaint: Int,
+        val rightMostPaint: Int,
+        val leftDestruction: Int,
+        val rightDesctruction: Int
+) {
+
+    fun canGoLeft(): Boolean {
+        return leftMostPaint >= 0
+                && (leftMostPaint - 1) > leftDestruction
+    }
+
+    fun canGoRight(): Boolean {
+        return rightMostPaint < stringEncoding.length - 1
+                && (rightMostPaint + 1) < rightDesctruction
+    }
+
+    fun canAdvanceLeftDestruction(): Boolean {
+        return leftDestruction + 1 < leftMostPaint
+    }
+
+    fun canAdvanceRightDestruction(): Boolean {
+        return rightDesctruction - 1 > rightMostPaint
+    }
+
+    fun goLeft(): PaintState {
+        val builder = StringBuilder(stringEncoding)
+        builder.setCharAt(leftMostPaint - 1, '*')
+        return PaintState(
+                builder.toString(),
+                leftMostPaint - 1,
+                rightMostPaint,
+                leftDestruction,
+                rightDesctruction
+        )
+    }
+
+    fun goRight(): PaintState {
+        val builder = StringBuilder(stringEncoding)
+        builder.setCharAt(rightMostPaint + 1, '*')
+        return PaintState(
+                builder.toString(),
+                leftMostPaint,
+                rightMostPaint + 1,
+                leftDestruction,
+                rightDesctruction
+        )
+    }
+
+    fun advanceLeftDestruction(): PaintState {
+        val builder = StringBuilder(stringEncoding)
+        builder.setCharAt(leftDestruction + 1, 'x')
+        return PaintState(
+                builder.toString(),
+                leftMostPaint,
+                rightMostPaint,
+                leftDestruction + 1,
+                rightDesctruction
+        )
+    }
+
+    fun advanceRightDestruction(): PaintState {
+        val builder = StringBuilder(stringEncoding)
+        builder.setCharAt(rightDesctruction - 1, 'x')
+        return PaintState(
+                builder.toString(),
+                leftMostPaint,
+                rightMostPaint,
+                leftDestruction,
+                rightDesctruction - 1
+        )
+    }
 }
 
 fun main(args: Array<String>) {
