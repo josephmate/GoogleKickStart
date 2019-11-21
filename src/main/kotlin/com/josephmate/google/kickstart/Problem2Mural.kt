ca9d4e91a911d541e6cfde69ad0db250ab81d470
Joseph Mate <mate.joseph@gmail.com>
2019-11-20 20:25:58 -0500

    naive solution implemented
    inefficient. can be improved with memoization. for instance painting at 0, then 1 is the same as painting 1 then 0. No need to re compute.

--- src/main/kotlin/com/josephmate/google/kickstart/Problem2Mural.kt
+++ src/main/kotlin/com/josephmate/google/kickstart/Problem2Mural.kt
@@ -11,14 +11,27 @@ class MuralSolver() {
         val scanner = Scanner(input);
         val numTestCases = scanner.nextInt();
         for(testCase in 1..numTestCases) {
-            val result = 0
             val numOfDigits = scanner.nextInt()
             scanner.nextLine() // read the rest of the line
             val line = scanner.nextLine()
             val problem = Problem(numOfDigits, line)
-            output.println("Case #$testCase: $result $line")
+            val max = findMax(problem)
+
+            output.println("Case #$testCase: $max")
         }
     }
+}
+
+fun findMax(problem: Problem): Int {
+    var max = 0;
+    for(paintPosn in 0 until problem.numOfDigits) {
+        val current = (iterateDestruction(problem.wallBeauty, encodeStart(problem.numOfDigits, paintPosn))
+            + calcBeauty(problem.wallBeauty, paintPosn))
+        if( current > max )  {
+            max = current
+        }
+    }
+    return max
 }
 
 fun encodeStart(numOfDigits: Int, paintPosn: Int): PaintState {
@@ -36,6 +49,46 @@ fun encodeStart(numOfDigits: Int, paintPosn: Int): PaintState {
             )
 }
 
+fun iterateDestruction(wallBeautyScore: String, paintState: PaintState): Int {
+    var max = 0;
+    if(paintState.canAdvanceLeftDestruction()) {
+        val current = iteratePaint(wallBeautyScore, paintState.advanceLeftDestruction())
+        if(current > max) {
+            max = current
+        }
+    }
+    if(paintState.canAdvanceRightDestruction()) {
+        val current = iteratePaint(wallBeautyScore, paintState.advanceRightDestruction())
+        if(current > max) {
+            max = current
+        }
+    }
+    return max
+}
+
+fun iteratePaint(wallBeautyScore: String, paintState: PaintState): Int {
+    var max = 0;
+    if(paintState.canPaintLeft()) {
+        val current = (iterateDestruction(wallBeautyScore, paintState.paintLeft())
+            + calcBeauty(wallBeautyScore, paintState.leftMostPaint - 1))
+        if(current > max) {
+            max = current
+        }
+    }
+    if(paintState.canPaintRight()) {
+        val current = (iterateDestruction(wallBeautyScore, paintState.paintRight())
+            + calcBeauty(wallBeautyScore, paintState.rightMostPaint + 1))
+        if(current > max) {
+            max = current
+        }
+    }
+    return max
+}
+
+fun calcBeauty(wallBeautyScore: String, posn: Int): Int {
+    return wallBeautyScore.get(posn).toInt() - 48
+}
+
 class Problem(val numOfDigits: Int, val wallBeauty: String) {
 }
 
@@ -47,12 +100,12 @@ class PaintState(
         val rightDesctruction: Int
 ) {
 
-    fun canGoLeft(): Boolean {
+    fun canPaintLeft(): Boolean {
         return leftMostPaint >= 0
                 && (leftMostPaint - 1) > leftDestruction
     }
 
-    fun canGoRight(): Boolean {
+    fun canPaintRight(): Boolean {
         return rightMostPaint < stringEncoding.length - 1
                 && (rightMostPaint + 1) < rightDesctruction
     }
@@ -65,7 +118,7 @@ class PaintState(
         return rightDesctruction - 1 > rightMostPaint
     }
 
-    fun goLeft(): PaintState {
+    fun paintLeft(): PaintState {
         val builder = StringBuilder(stringEncoding)
         builder.setCharAt(leftMostPaint - 1, '*')
         return PaintState(
@@ -77,7 +130,7 @@ class PaintState(
         )
     }
 
-    fun goRight(): PaintState {
+    fun paintRight(): PaintState {
         val builder = StringBuilder(stringEncoding)
         builder.setCharAt(rightMostPaint + 1, '*')
         return PaintState(
