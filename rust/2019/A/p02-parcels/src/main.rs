a95eb9c2e98f15ef2d2452a155455681f9f0336e
Joseph Mate <mate.joseph@gmail.com>
2020-08-18 22:59:03 -0400

    works, but too slow for google test sets

--- rust/2019/A/p02-parcels/src/main.rs
+++ rust/2019/A/p02-parcels/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 
 // naive solution that attempts all potentials post offices
 // for each potential post office compute the score
@@ -10,18 +11,45 @@ use std::collections::HashSet;
 //  \ /     \ /     BFS to find nearest post office (could start from one corner and post office in opposite corner)
 //   |       Finds the manning distance for each position 
 //   Trying every possible post office position
+// This solution will be too slow for the large data set, but I'm using it as a starting point.
 
 fn bfs_post_office_impl(
-    current_row: usize,
-    current_column: usize,
+    start_row: usize,
+    start_column: usize,
     rows: usize,
     columns: usize,
     grid: & Vec<Vec<i32>>,
-    visited: &mut HashSet<(i32, i32)>
 ) -> Option<(usize, usize)> {
-    if grid[current_row][current_col] == 1 {
-        return Some(current_row, current_col);
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut bfs_queue: VecDeque<(usize, usize)> = VecDeque::new();
+    bfs_queue.push_back((start_row, start_column));
+
+    loop {
+        match bfs_queue.pop_front() {
+            Some((current_row, current_column)) => {
+                if grid[current_row][current_column] == 1 {
+                    return Some((current_row, current_column));
+                }
+                visited.insert((current_row, current_column));
+
+                if current_row + 1 < rows {
+                    bfs_queue.push_back((current_row + 1, current_column));
+                }
+                if current_row as i32 - 1 >= 0 {
+                    bfs_queue.push_back((current_row - 1, current_column));
+                }
+                if current_column + 1 < columns {
+                    bfs_queue.push_back((current_row, current_column + 1));
+                }
+                if current_column as i32 - 1 >= 0 {
+                    bfs_queue.push_back((current_row, current_column - 1));
+                }
+            },
+            None => {break;}
+        }
     }
+
+    return None;
 }
 
 fn bfs_post_office(
@@ -31,7 +59,8 @@ fn bfs_post_office(
     columns: usize,
     grid: &mut Vec<Vec<i32>>
 ) -> (usize, usize) {
-    bfs_post_office_impl(start_row, start_column, rows, columns, &grid, &mut HashSet::new())
+    bfs_post_office_impl(start_row, start_column, rows, columns, grid)
+        .expect("There should be at least one post office in the grid, so something must be found.")
 }
 
 // The Manhattan distance between two squares (r1,c1) and (r2,c2) is defined as |r1 - r2| + |c1 - c2|, where |*| operator denotes the absolute value.
@@ -55,7 +84,10 @@ fn score_position(
     for r in 0..rows {
         for c in 0..columns {
             let (nearest_row, nearest_col) = bfs_post_office(r, c, rows, columns, grid);
-            score_so_far += manhattan_distance(r, c, nearest_row, nearest_col);
+            let distance  = manhattan_distance(r, c, nearest_row, nearest_col);
+            if distance > score_so_far {
+                score_so_far = distance;
+            }
         }
     }
 
@@ -67,7 +99,7 @@ fn solve(
     columns: usize,
     grid: &mut Vec<Vec<i32>>
 ) {
-    let mut min_so_far = std::i32::MAX;
+    let mut min_so_far = None;
     for r in 0..rows {
         for c in 0..columns {
             if grid[r][c] == 0 {
@@ -78,13 +110,29 @@ fn solve(
                 // change it back
                 grid[r][c] = 0;
 
-                if score < min_so_far {
-                    min_so_far = score;
+                match min_so_far {
+                    Some(min) => {
+                        if score < min {
+                            min_so_far = Some(score);
+                        }
+                    },
+                    None => {
+                        min_so_far = Some(score)
+                    }
                 }
+                
             }
         }
     }
-    println!("{}", min_so_far)
+
+    match min_so_far {
+        Some(min) => {
+            println!("{}", min)
+        },
+        None => {
+            println!("{}", 0)
+        }
+    }
 }
 
 // The first line of the input gives the number of test cases, T. T test cases follow.
@@ -113,7 +161,7 @@ fn handle_rows_and_columns(rows: usize, columns: usize) {
                 grid.push(row);
             }
             Err(io_error) => {
-                print!("{}", io_error);
+                print!("io error: {}", io_error);
                 return;
             }
         }
