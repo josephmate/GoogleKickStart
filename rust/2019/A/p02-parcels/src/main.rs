c73ed88e54c61b3db1469927210e802e4367d815
Joseph Mate <mate.joseph@gmail.com>
2020-08-21 23:07:25 -0400

    improve complexity, but still not good enough for the large test case

--- rust/2019/A/p02-parcels/src/main.rs
+++ rust/2019/A/p02-parcels/src/main.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::collections::HashSet;
 use std::collections::VecDeque;
 
 // naive solution that attempts all potentials post offices
@@ -68,67 +67,85 @@ use std::collections::VecDeque;
 //               D(M,X) > D(X,B)
 //      D(M,m) > D(X,x) > D(X,B)
 //      D(M,m) > D(M,B)
-// TODO continue proof
 
-fn bfs_post_office_impl(
-    start_row: usize,
-    start_column: usize,
+// Maybe it's not true. Let me try finding a counter example
+//    434  
+//    323
+//    212
+//    101
+// Placing at max does not give the best solution.
+//    012  
+//    123
+//    212
+//    101
+// This one is the best solution.
+//    101  
+//    212
+//    212
+//    101
+// Maybe we have to try every position?
+// With the score calculating optimization, getting rid of the BFS results in
+//
+// N * N + N * N * N * N
+// |   |   |   |   \   /
+// |   |   \   /     Brute forcing each possible square
+// \   /    Calculating the score, from the inital score map when adding a post office.
+// Calculating the initial score map
+// 250^4 ~ 10 * 10^8 = 10^9 = 1,000,000,000
+
+
+fn bfs_score(
     rows: usize,
     columns: usize,
     grid: & Vec<Vec<i32>>,
-) -> Option<(usize, usize)> {
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    let mut bfs_queue: VecDeque<(usize, usize)> = VecDeque::new();
-    bfs_queue.push_back((start_row, start_column));
+) -> Vec<Vec<i32>> {
+    // create an empty grid of scores
+    // -1 indicates no score yet
+    let mut scores = Vec::new();
+    for r in 0..rows {
+        let mut row = Vec::new();
+        for c in 0..columns {
+            row.push(std::i32::MAX);
+        }
+        scores.push(row);
+    }
 
-    loop {
-        match bfs_queue.pop_front() {
-            Some((current_row, current_column)) => {
-                if grid[current_row][current_column] == 1 {
-                    return Some((current_row, current_column));
-                }
-                visited.insert((current_row, current_column));
-
-                if current_row + 1 < rows {
-                    bfs_queue.push_back((current_row + 1, current_column));
-                }
-                if current_row as i32 - 1 >= 0 {
-                    bfs_queue.push_back((current_row - 1, current_column));
-                }
-                if current_column + 1 < columns {
-                    bfs_queue.push_back((current_row, current_column + 1));
-                }
-                if current_column as i32 - 1 >= 0 {
-                    bfs_queue.push_back((current_row, current_column - 1));
-                }
-            },
-            None => {break;}
+    // get all the post offices in the queue
+    let mut bfs_queue: VecDeque<(usize, usize, usize)> = VecDeque::new();
+    for r in 0..rows {
+        for c in 0..columns {
+            if grid[r][c] == 1 {
+                bfs_queue.push_back((r,c,0));
+            }
         }
     }
 
-    return None;
-}
+    loop {
+        match bfs_queue.pop_front() {
+            Some((current_row, current_column, score)) => {
+                if (score as i32) < scores[current_row][current_column] {
+                    scores[current_row][current_column] = score as i32;
+                    if current_row + 1 < rows {
+                        bfs_queue.push_back((current_row + 1, current_column, score + 1));
+                    }
+                    if current_row as i32 - 1 >= 0 {
+                        bfs_queue.push_back((current_row - 1, current_column, score + 1));
+                    }
+                    if current_column + 1 < columns {
+                        bfs_queue.push_back((current_row, current_column + 1, score + 1));
+                    }
+                    if current_column as i32 - 1 >= 0 {
+                        bfs_queue.push_back((current_row, current_column - 1, score + 1));
+                    }
+                }
+            },
+            None => {
+                break;
+            }
+        }
+    }
 
-fn bfs_post_office(
-    start_row: usize,
-    start_column: usize,
-    rows: usize,
-    columns: usize,
-    grid: &mut Vec<Vec<i32>>
-) -> (usize, usize) {
-    bfs_post_office_impl(start_row, start_column, rows, columns, grid)
-        .expect("There should be at least one post office in the grid, so something must be found.")
-}
-
-// The Manhattan distance between two squares (r1,c1) and (r2,c2) is defined as |r1 - r2| + |c1 - c2|, where |*| operator denotes the absolute value.
-fn manhattan_distance(
-    start_row: usize,
-    start_column: usize,
-    end_row: usize,
-    end_column: usize,
-) -> i32 {
-    (start_row as i32 - end_row as i32).abs()
-    + (start_column as i32 - end_column as i32).abs()
+    return scores;
 }
 
 fn score_position(
@@ -136,19 +153,16 @@ fn score_position(
     columns: usize,
     grid: &mut Vec<Vec<i32>>
 ) -> i32 {
-    let mut score_so_far = 0;
-
+    let scores = bfs_score(rows, columns, grid);
+    let mut max_so_far = 0;
     for r in 0..rows {
         for c in 0..columns {
-            let (nearest_row, nearest_col) = bfs_post_office(r, c, rows, columns, grid);
-            let distance  = manhattan_distance(r, c, nearest_row, nearest_col);
-            if distance > score_so_far {
-                score_so_far = distance;
+            if scores[r][c] > max_so_far {
+                max_so_far = scores[r][c];
             }
         }
     }
-
-    return score_so_far;
+    return max_so_far;
 }
 
 fn solve(
@@ -156,6 +170,7 @@ fn solve(
     columns: usize,
     grid: &mut Vec<Vec<i32>>
 ) {
+
     let mut min_so_far = None;
     for r in 0..rows {
         for c in 0..columns {
