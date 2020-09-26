cdb13d9fe3b322d80044b99799bb4311b5bbe6a7
Joseph Mate <mate.joseph@gmail.com>
2020-09-25 21:26:51 -0400

    parsing Round E Problem 3 - Yogurt

---
+++ java/2018/E/p03-yogurt/src/Solution.java
@@ -0,0 +1,134 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    private long solve(
+            long first,
+            long second,
+            List<Long> values
+    ) {
+        long result = 0;
+        return result;
+    }
+
+    private void handleTestCase(int testCase) throws IOException {
+        writer.write("Case #" + testCase + ": ");
+        Pair<Long,Long> pair = parsePairLongLine();
+        List<Long> values = parseLongListLine();
+        long result = solve(
+                pair.first,
+                pair.second,
+                values
+        );
+        writer.write(String.valueOf(result));
+        writer.write("\n");
+    }
+
+    public void parseAndSolveProblems() throws IOException {
+        long testCases = parseLongLine();
+        for (int i = 1; i <= testCases; i++) {
+            handleTestCase(i);
+        }
+    }
+
+    private List<Long> parseLongListLine() throws IOException {
+        try {
+            String line = reader.readLine();
+            String [] columns = line.split(" ");
+            List<Long> result = new ArrayList<>(columns.length);
+            for (int i = 0; i < columns.length; i++) {
+                result.add(Long.parseLong(columns[i]));
+            }
+            currentLine++;
+            return result;
+        } catch (RuntimeException e) {
+            System.err.println("Was expecting line " + currentLine + " to have many Longs");
+            throw e;
+        } catch (IOException e) {
+            System.err.println("Could not read line " + currentLine);
+            throw e;
+        }
+    }
+
+    private String parseStringLine() throws IOException {
+        try {
+            String line = reader.readLine();
+            currentLine++;
+            return line;
+        } catch (RuntimeException e) {
+            System.err.println("Was expecting line " + currentLine + " to fit into a Long");
+            throw e;
+        } catch (IOException e) {
+            System.err.println("Could not read line " + currentLine);
+            throw e;
+        }
+    }
+
+    private static final class Pair<X, Y> {
+        public final X first;
+        public final Y second;
+
+        public Pair(X first, Y second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    private List<Pair<Long,Long>> parseManyLongPairs(int numPairs) throws IOException {
+        List<Pair<Long,Long>> result = new ArrayList<>(numPairs);
+        for (long i = 0; i < numPairs; i++) {
+            result.add(parsePairLongLine());
+        }
+        return result;
+    }
+
+    private Pair<Long, Long> parsePairLongLine() throws IOException {
+        try {
+            String line = reader.readLine().trim();
+            String [] columns = line.split(" ");
+            long first = Long.parseLong(columns[0]);
+            long second = Long.parseLong(columns[1]);
+            currentLine++;
+            return new Pair<>(first, second);
+        } catch (RuntimeException e) {
+            System.err.println("Was expecting line " + currentLine + " to fit into a Long");
+            throw e;
+        } catch (IOException e) {
+            System.err.println("Could not read line " + currentLine);
+            throw e;
+        }
+    }
+
+    private long parseLongLine() throws IOException {
+        try {
+            long result = Long.parseLong(reader.readLine().trim());
+            currentLine++;
+            return result;
+        } catch (RuntimeException e) {
+            System.err.println("Was expecting line " + currentLine + " to fit into a Long");
+            throw e;
+        } catch (IOException e) {
+            System.err.println("Could not read line " + currentLine);
+            throw e;
+        }
+    }
+
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+    private long currentLine = 1;
+
+    public Solution(
+            BufferedReader reader,
+            BufferedWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            new Solution(reader, writer).parseAndSolveProblems();
+        }
+    }
+}
