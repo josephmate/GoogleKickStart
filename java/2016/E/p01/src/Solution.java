2dae80af2965443dc03060eb4b7482bf27dbe79f
Joseph Mate <mate.joseph@gmail.com>
2021-04-24 17:55:26 -0400

    close, but there's a bug

--- java/2016/E/p01/src/Solution.java
+++ java/2016/E/p01/src/Solution.java
@@ -7,19 +7,87 @@ import java.util.stream.Stream;
 
 public class Solution {
 
+    private int countBs(
+            String pattern
+    ) {
+        return countBs(pattern, 0, pattern.length()-1);
+    }
+
+    private int countBs(
+            String pattern,
+            int start,
+            int end
+    ) {
+        int numOfBs = 0;
+        int index = start;
+        while(true) {
+            char currentChar = pattern.charAt(index);
+            if (currentChar == 'B') {
+                numOfBs++;
+            }
+
+            if (index == end) {
+                break;
+            }
+
+            index = (index + 1) % pattern.length();
+        }
+        return numOfBs;
+    }
+
+    /**
+     * Solution A
+     * c from i to j  increment if pattern[c % L] == 'B'
+     * Since problem size of i and j is 10^18 about 2^60. Although this solution will be easier to
+     * implement, this is at the limit of brute forcing the calculation. So we should go with Solution B.
+     *
+     * Solution A
+     * L length of string
+     * 1) b number of B in pattern
+     * 2) Figure out which section of pattern is in the remainder N%L
+     * There will be some characters at the front and end that don't fit:
+     *
+     * 1 2 3 4 5 6 7 9 10
+     * A B C A B C A B C
+     *   |         |
+     *   ==========
+     *
+     * Instead of determining the characters at the front and end that don't fit. We can rotate
+     * the pattern so it matches the start at i. Then we can figure out how many characters to cut
+     * off the end.
+     *
+     * @param pattern
+     * @param i
+     * @param j
+     * @return
+     */
     private String solve(
             String pattern,
-            long i,
-            long j
+            int i,
+            int j
     ) {
-        return "";
+        // 0 index
+        i = i - 1;
+        j = j - 1;
+
+        int numOfBsInPattern = countBs(pattern);
+        int fullPatterns = (j - i + 1)/pattern.length();
+        int totalBs = fullPatterns*numOfBsInPattern;
+
+        if ( (j - i + 1)%pattern.length() > 0 ) {
+            int patternStart = i % pattern.length();
+            int patternEnd = j % pattern.length();
+
+        }
+
+        return String.valueOf(totalBs);
     }
 
     private void handleTestCase(int testCase) throws IOException {
         writer.write("Case #" + testCase + ": ");
         String line = parseStringLine();
         Pair<Long, Long> pair = parsePairLongLine();
-        writer.write(solve(line, pair.first, pair.second));
+        writer.write(solve(line, pair.first.intValue(), pair.second.intValue()));
         writer.write("\n");
     }
 
@@ -48,6 +116,7 @@ public class Solution {
             throw e;
         }
     }
+
     private List<String> parseManyStringLine(int numStrings) throws IOException {
         List<String> result = new ArrayList<>(numStrings);
         for (long i = 0; i < numStrings; i++) {
