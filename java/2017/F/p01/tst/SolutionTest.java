5f568266bd3c5ca35c3b2fc81a58ec337d822eed
Joseph Mate <mate.joseph@gmail.com>
2021-04-25 20:33:59 -0400

    found counter example that shows greedy solution doesn't work 3*3 + 3*3

--- java/2017/F/p01/tst/SolutionTest.java
+++ java/2017/F/p01/tst/SolutionTest.java
@@ -12,7 +12,23 @@ class SolutionTest {
     }
 
     @Test
-    public void test() {
+    public void testPerfectSquares() {
+        for (int i = 1; i <= 1000; i++) {
+            assertEquals(1, Solution.solve(i*i));
+        }
     }
 
+    @Test
+    public void testPairOfSquares() {
+        assertEquals(2, Solution.solve(3*3 + 3*3));
+    }
+
+    @Test
+    public void testManyPairsOfSquares() {
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = 1; j <= 1000; j++) {
+                assertEquals(2, Solution.solve(i*i + j*j), i + " " + j);
+            }
+        }
+    }
 }
