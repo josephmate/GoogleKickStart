7298eb5c2bf3eaff04423dea889d9d1596bc9787
Joseph Mate <mate.joseph@gmail.com>
2021-04-25 20:10:58 -0400

    2017 F p01: parsed input

---
+++ java/2017/F/p01/tst/SolutionTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    public void testSample() {
+        assertEquals(3, Solution.solve(3));
+        assertEquals(1, Solution.solve(4));
+        assertEquals(2, Solution.solve(5));
+    }
+
+    @Test
+    public void test() {
+    }
+
+}
