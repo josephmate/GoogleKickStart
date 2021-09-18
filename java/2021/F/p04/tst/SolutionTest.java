c9870e87bdc0e8cacae954458940020e24d6f726
Joseph <mate.joseph@gmail.com>
2021-09-18 12:11:05 -0400

    Ready for Sept 18 2021 1pm EST contest

---
+++ java/2021/F/p04/tst/SolutionTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+  @Test
+  public void test() {
+
+  }
+
+}
